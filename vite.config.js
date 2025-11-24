1debd28868b4384108bce7c05a86dc31c2f122c0
Nicolas <nicogaveglio@gmail.com>
2025-11-24 08:01:25 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GASTOS-COMPARTIDOS/', // ⬅️ Reemplaza con el nombre de tu repo
+  base: '/gastos-compartidos/', // ⬅️ Reemplaza con el nombre de tu repo
 })
