7c4f092675d9f8696f1dc023990ccb3e119a355a
Nicolas <nicogaveglio@gmail.com>
2025-12-29 20:31:59 +0100

    Merge remote-tracking branch 'origin/main'

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ntwxufxswhjrgdmtqjlt.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50d3h1Znhzd2hqcmdkbXRxamx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNzQyMDQsImV4cCI6MjA3Nzk1MDIwNH0.wJFqOWMSM9j9UBneJT9-u_H-GyZlnN-wFsY6c8IM08k'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
