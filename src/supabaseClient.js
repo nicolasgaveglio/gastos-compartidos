b8ce50817fb926d3bab8d308b3c0e932a503aedf
Nicolas <nicogaveglio@gmail.com>
2025-12-29 08:11:18 +0100

    🔑 GitHub Pages - Supabase keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://ntwxufxswhjrgdmtqjlt.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50d3h1Znhzd2hqcmdkbXRxamx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNzQyMDQsImV4cCI6MjA3Nzk1MDIwNH0.wJFqOWMSM9j9UBneJT9-u_H-GyZlnN-wFsY6c8IM08k'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
