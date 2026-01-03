7f2a2e8f6f510c7ce13377b59ff084ffcabf13e3
Nicolas <nicogaveglio@gmail.com>
2026-01-03 22:34:53 +0100

    Build para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Comenta o quita el base para desarrollo local
-  // base: '/gastos-compartidos/',
+  base: '/gastos-compartidos/',
   server: {
-    port: 3000,
-    open: true,
-  },
-});
+    port: 3000
+  }
+})
